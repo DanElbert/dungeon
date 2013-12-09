de291ee478abc2a75447fbc4562b5bdba02f51da
Dan Elbert <dan.elbert@gmail.com>
2013-12-08 19:49:37 -0600

    Fixed image loading in game edit

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  def get_background_image_options(selected)
+    get_board_backgrounds.map do |name, path|
+      selected = selected == path
+      url =  ActionController::Base.helpers.asset_path(path)
+      "<option#{selected ? ' selected' : ''} data-img-src='#{url}' value='#{path}'>#{name}</option>"
+    end.join("\n").html_safe
+  end
 end
