4f330c20bb19633c744f61484e19f924b9a73184
Dan Elbert <dan.elbert@gmail.com>
2014-07-06 15:27:17 -0500

    Fixed migration

--- db/migrate/20140706191159_migrate_boards_background_image.rb
+++ db/migrate/20140706191159_migrate_boards_background_image.rb
@@ -4,17 +4,16 @@ class MigrateBoardsBackgroundImage < ActiveRecord::Migration
   end
 
   def up
-    BackgroundImage.reset_column_information
-    Board.reset_column_information
+    Image.reset_column_information
 
     Dir[Rails.root.join('db', 'seed_data', 'background_images', '*')].each do |file|
       name = File.basename file
-      unless BackgroundImage.where(filename: name, type: 'BackgroundImage').exists?
+      unless Image.where(filename: name, type: 'BackgroundImage').exists?
         Image.create!({filename: name, type: 'BackgroundImage', data: File.binread(file)})
       end
     end
 
-    bgs = BackgroundImage.all.to_a
+    bgs = Image.where(type: 'BackgroundImage').to_a
 
     bgs.each do |bg|
       old_name = "board/backgrounds/#{bg.filename}"
