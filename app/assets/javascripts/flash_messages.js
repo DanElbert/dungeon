07ec8bd85d04eab4b435afa8120d95565d45b6a0
dan.elbert@gmail.com <dan.elbert@gmail.com>
2015-04-08 15:31:21 -0500

    Fixed flash messages; updated forms

--- app/assets/javascripts/flash_messages.js
+++ app/assets/javascripts/flash_messages.js
@@ -1,16 +1,31 @@
 
+var flashMessageTypeMap = {
+  error: "danger",
+  notice: "success"
+};
+
 function flashMessage(flashType, message) {
 
   var timeoutIdContainer = {};
 
+  if (flashMessageTypeMap[flashType]) {
+    flashType = flashMessageTypeMap[flashType];
+  }
+
+  var closeButton = $("<button type='button' />")
+      .addClass("close")
+      .append($("<span />").html("&times;"))
+      .bind("click.Flash", function() { $(this).parent().hide({effect: "fade", duration: 1000}); clearTimeout(timeoutIdContainer.id); });
+
   var $flashDiv = $("<div></div>")
       .html(message)
-      .addClass(flashType)
-      .addClass("flash")
+      .append(closeButton)
+      .addClass("popup")
+      .addClass("alert")
+      .addClass("alert-" + flashType)
       .hide()
       .appendTo("#flashContainer")
-      .show({effect: "pulsate", times: 1, duration: 1500})
-      .bind("click.Flash", function() { $(this).hide({effect: "fade", duration: 1000}); clearTimeout(timeoutIdContainer.id); });
+      .show({effect: "pulsate", times: 1, duration: 1500});
 
   timeoutIdContainer.id = setTimeout(function() {
     $flashDiv.unbind(".Flash");
