d54caebf24f15392ba7c87ab3da6bc477943cbe7
Dan Elbert <dan.elbert@gmail.com>
2020-04-25 23:27:42 -0500

    ruby and rails upgrade

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module Dungeon
 
     config.middleware.insert_after Rack::Sendfile, Rack::Deflater
 
+    config.load_defaults "6.0"
+
     Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,3 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-ENV['EXECJS_RUNTIME'] = 'Node'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
